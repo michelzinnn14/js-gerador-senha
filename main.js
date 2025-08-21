9eaf23045de0097ae1827d3ea91d555ea648a951
michelzinnn14 <163421867+michelzinnn14@users.noreply.github.com>
2025-08-21 07:31:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-     tamanhoSenha = tamanhoSenha - 1;
+      tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
@@ -17,43 +17,52 @@ function diminuiTamanho(){
 
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-     tamanhoSenha = tamanhoSenha + 1;
+      tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-for(i = 0; < checkbox.lengh; i++){
-    checkbox[i].onclick = gerasenha;
+for(let i = 0; i < checkbox.length; i++){
+    checkbox[i].onclick = geraSenha;
 }
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-geraSenha();
-
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const LetrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
 
-function geraSenha(){
-    let senha = '';
-    for (let i = 0; i < tamanhoSenha; i++){
-    let numeroAleatorio = Math.random() * letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
 
-        if (checkbox[3].checkd){
+geraSenha();
+
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
         alfabeto = alfabeto + letrasMinusculas;
-        }
-        if (checkbox[3].checkd){
+    }
+    if (checkbox[2].checked){
         alfabeto = alfabeto + numeros;
-        }
-        if (checkbox[3].checkd){
+    }
+    if (checkbox[3].checked){
         alfabeto = alfabeto + simbolos;
-        }
-        
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    
+    // Adicione esta verificação para garantir que o alfabeto não esteja vazio
+    if (alfabeto === '') {
+        alfabeto = letrasMaiusculas + letrasMinusculas;
+    }
+
+    let senha = '';
+    for (let i = 0 ; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
 }
